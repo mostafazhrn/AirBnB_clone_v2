e388bf39aa00fa555183775e2cf7b79ae5868cd8
mostafazhrn <mostafazhrn@gmail.com>
2024-03-07 14:41:05 -0500

    deploy_web_static_fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -14,11 +14,11 @@ def do_pack():
     """This instance shall pack web_static into tgz with ymdhr format"""
     date = datetime.utcnow()
     fiche = "versions/web_static_{}{}{}{}{}{}.tgz".format(date.year,
-                                                            date.month,
-                                                            date.day,
-                                                            date.hour,
-                                                            date.minute,
-                                                            date.second)
+                                                          date.month,
+                                                          date.day,
+                                                          date.hour,
+                                                          date.minute,
+                                                          date.second)
     res = local("mkdir -p versions")
     res = local("tar -cvzf " + fiche + " web_static")
     if res.failed:
@@ -27,29 +27,39 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """ THis instance shall dist the arch to web_servers"""
+    """THis instance shall dist the archive of web_stat to the web servers"""
     if os.path.isfile(archive_path) is False:
         return False
-    try:
-        arch = archive_path.split("/")[-1]
-        pth = "/data/web_static/releases/"
-        put("{}".format(archive_path), "/tmp/{}".format(arch))
-        run("sudo mkdir -p {}{}/".format(pth, arch[:-4]))
-        run("sudo tar -xzf /tmp/{} -C {}{}/".format(arch, pth, arch[:-4]))
-        run("sudo rm /tmp/{}".format(arch))
-        run("sudo mv {0}{1}/web_static/* {0}{1}/"
-            .format(pth, arch[:-4]))
-        run("sudo rm -rf {}{}/web_static".format(pth, arch[:-4]))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {}{}/ /data/web_static/current".format(pth, arch[:-4]))
-        return True
-    except:
+    fiche = archive_path.split("/")[-1]
+    nom = fiche.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(fiche)).failed is True:
         return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(nom)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(nom)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(fiche, nom)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(fiche)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(nom, nom)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(nom)).failed is True:
+        return False
+    return True
 
 
 def deploy():
-    """ THis shall deplay the files of the web_static"""
-    path = do_pack()
-    if path is None:
+    """This instance shall deploy the web_static """
+    fiche = do_pack()
+    if fiche is None:
         return False
-    return do_deploy(path)
+    return do_deploy(fiche)
