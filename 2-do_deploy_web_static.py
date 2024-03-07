47df4f2d7e7b05be2ac56d7db9cb516d6b58130b
mostafazhrn <mostafazhrn@gmail.com>
2024-03-07 13:45:26 -0500

    fix_2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
-"""This fabric script shall dist archive made at first task to web_servers"""
+"""Fabric script that distributes an archive to web servers,
+using the function do_deploy.
+"""
 from fabric.api import local, env, put, run
 import os
 from os.path import exists
+import datetime
+
 env.hosts = ['54.174.72.190', '100.26.218.108']
 
 
 def do_deploy(archive_path):
-    """This instance shall dist, unpack then deploy archives to web_servers"""
-    if exists(archive_path) is False:
+    """THis shall dist the archive to the web servers"""
+    if os.path.isfile(archive_path) is False:
         return False
     try:
-        put(archive_path, "/tmp/")
-        nom = archive_path.split("/")[1].split(".")[0]
-        run("mkdir -p /data/web_static/releases/{}/".format(nom))
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-            .format(nom, nom))
-        run("rm /tmp/{}.tgz".format(nom))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/
-        releases/{}/".format(nom, nom))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(nom))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(nom))
+        arch = archive_path.split("/")[-1]
+        pth = "/data/web_static/releases/"
+        put("{}".format(archive_path), "/tmp/{}".format(arch))
+        run("sudo mkdir -p {}{}/".format(pth, arch[:-4]))
+        run("sudo tar -xzf /tmp/{} -C {}{}/".format(arch, pth, arch[:-4]))
+        run("sudo rm /tmp/{}".format(arch))
+        run("sudo mv {0}{1}/web_static/* {0}{1}/"
+            .format(pth, arch[:-4]))
+        run("sudo rm -rf {}{}/web_static".format(pth, arch[:-4]))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {}{}/ /data/web_static/current".format(pth, arch[:-4]))
         return True
     except:
         return False
