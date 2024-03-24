bc3e4bcb4a24e3226e6135b091b7684489369d27
mostafazhrn <mostafazhrn@gmail.com>
2024-03-24 13:04:02 -0400

    db_storage_fix1

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -12,6 +12,9 @@ from models.review import Review
 import os
 from os import getenv
 
+classes = {"State": State, "City": City, "User": User, "Place": Place,
+           "Review": Review, "Amenity": Amenity}
+
 
 class DBStorage:
     """This is the DBStorage class"""
@@ -20,12 +23,12 @@ class DBStorage:
 
     def __init__(self):
         """This shall initialize the DBStorage"""
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                      format(getenv("HBNB_MYSQL_USER"),
-                                             getenv("HBNB_MYSQL_PWD"),
-                                             getenv("HBNB_MYSQL_HOST"),
-                                             getenv("HBNT_MYSQL_DB")),
-                                        pool_pre_ping=True)
+        database = "{}://{}:{}@{}:3306/{}".format("mysql+mysqldb",
+                                                  getenv("HBNB_MYSQL_USER"),
+                                                  getenv("HBNB_MYSQL_PWD"),
+                                                  getenv("HBNB_MYSQL_HOST"),
+                                                  getenv("HBNB_MYSQL_DB"))
+        self.__engine = create_engine(database, pool_pre_ping=True)
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
             
